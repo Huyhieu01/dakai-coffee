01b649bddcf56e8d3fb3225ffdeed25bd095c48b
Huyhieu01 <mino010901@gmail.com>
2024-11-01 17:08:56 +0700

    Upload

--- src/pages/index/banner.tsx
+++ src/pages/index/banner.tsx
@@ -1,32 +1,35 @@
-import React, { FC } from "react";
-import { Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { getDummyImage } from "utils/product";
-import { Box } from "zmp-ui";
+import React, { FC } from "react";  
+import { Pagination } from "swiper";  
+import { Swiper, SwiperSlide } from "swiper/react";  
+import { Box } from "zmp-ui";  
 
-export const Banner: FC = () => {
-  return (
-    <Box className="bg-white" pb={4}>
-      <Swiper
-        modules={[Pagination]}
-        pagination={{
-          clickable: true,
-        }}
-        autoplay
-        loop
-        cssMode
-      >
-        {[1, 2, 3, 4, 5]
-          .map((i) => getDummyImage(`banner-${i}.webp`))
-          .map((banner, i) => (
-            <SwiperSlide key={i} className="px-4">
-              <Box
-                className="w-full rounded-lg aspect-[2/1] bg-cover bg-center bg-skeleton"
-                style={{ backgroundImage: `url(${banner})` }}
-              />
-            </SwiperSlide>
-          ))}
-      </Swiper>
-    </Box>
-  );
+export const Banner: FC = () => {  
+  const images = [  
+    "https://res.cloudinary.com/dqcrcdufy/image/upload/v1730430808/1_tr18s6.png",  
+    "https://res.cloudinary.com/dqcrcdufy/image/upload/v1730430808/2_nk32ja.png", // Thay đổi link ảnh 2  
+    "https://res.cloudinary.com/dqcrcdufy/image/upload/v1730430808/3_cdakoq.png"  // Thay đổi link ảnh 3  
+  ];  
+
+  return (  
+    <Box className="bg-white" pb={4}>  
+      <Swiper  
+        modules={[Pagination]}  
+        pagination={{  
+          clickable: true,  
+        }}  
+        autoplay  
+        loop  
+        cssMode  
+      >  
+        {images.map((image, i) => (  
+          <SwiperSlide key={i} className="px-4">  
+            <Box  
+              className="w-full rounded-lg aspect-[2/1] bg-cover bg-center bg-skeleton"  
+              style={{ backgroundImage: `url(${image})` }}  
+            />  
+          </SwiperSlide>  
+        ))}  
+      </Swiper>  
+    </Box>  
+  );  
 };
